05b28fa3af10dad8066f9ccea76618ac162699f2
MHRipon01 <136681772+MHRipon01@users.noreply.github.com>
2023-09-02 21:23:27 +0600

    almost project completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let fetchData;
 
 const handleCategory = async () => {
   const res = await fetch(
@@ -27,20 +28,40 @@ const handleCategory = async () => {
   // console.log(handleCategory());
 };
 
-let fetchedVideos = [];
+// let fetchedVideos = [];
 const handleClass = async (categoryId) => {
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
-  );
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
 
   const data = await res.json();
-  console.log(data.data.length);
-  //  fetchedVideos = data.data
+  videoData = data.data;
+  // console.log(data.data);
+  
   //  console.log(fetchedVideos);
+  
+  // function sortMaker() {
+  //   // let result = fetchedVideos.sort();
+  //   fetchd = fetchData
+  // }
 
-  //  function sortMaker(){
-  //   let result = fetchedVideos.sort()
-  //  }
+  //sort 
+  function sortData(){
+    fetchData = videoData;
+    console.log(fetchData);
+
+    fetchData.sort((a, b) => b.others.views - a.others.views
+      );
+    console.log(fetchData);
+    
+  }
+
+  const sortByViewButton = document.querySelector("#sort-by-view");
+// console.log(sortByViewButton);
+sortByViewButton.addEventListener("click", () => {
+ sortData()
+  // console.log();
+});
+
 
   if (data.data.length === 0) {
     const drawing = document.getElementById("drawing");
@@ -56,7 +77,7 @@ const handleClass = async (categoryId) => {
   data.data.forEach((videos) => {
     const check = parseFloat(videos?.others?.posted_date);
 
-    console.log(videos);
+    // console.log(videos);
 
     const timeConverter = (second) => {
       if (isNaN(second)) {
@@ -108,103 +129,30 @@ const handleClass = async (categoryId) => {
   });
 };
 
-//     const sortByViewButton = document.querySelector('#sort-by-view');
-// sortByViewButton.addEventListener('click', () => {
-
-// })
-
-// let fetchedVideos = []
-// const handleClass = async (categoryId) => {
-//   const res = await fetch(
-//     `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
-//   );
-
-//   const data = await res.json();
-//   fetchedVideos = data.data;
-
-//   if (data.data.length === 0) {
-//     const drawing = document.getElementById('drawing')
-//     drawing.classList.remove('hidden')
-//   } else {
-//     const drawing = document.getElementById('drawing');
-//     drawing.classList.add('hidden');
-//   }
-
-//   displayVideos(fetchedVideos);
-// };
-
-// const displayVideos = (videos) => {
-//   const cardContainer = document.getElementById("card-container");
-//   cardContainer.innerHTML = "";
-//   videos.forEach((video) => {
-//     const check = parseFloat(video?.others?.posted_date)
-
-//     const timeConverter = (second) => {
-//       if (isNaN(second)) {
-//         return '';
-//       }
-//       const hoursWithoutDecimal = Math.floor(second / 3600);
-//       const minutes = Math.floor((second % 3600) / 60);
-//       return `${hoursWithoutDecimal} hrs ${minutes} minutes ago`;
-//     }
-
-//     const div = document.createElement("div");
-//     const convertedTime = parseFloat(video?.others?.posted_date ? check : ``);
-//     div.innerHTML = `
-//         <div class="card md:w-full md:p-14 lg:p-1 lg:w-full lg:h-full  bg-base-100 ">
-//         <figure class=" pt-2">
-//           <img class="shadow-xl rounded-xl w-full m-5 lg:p-1 lg:w-full lg:h-[347px] " src="${
-//       video.thumbnail
-//       }" alt="Shoes" class="rounded-xl" />
-//         </figure>
-//         <div class="card-body items-center text-center">
-//             <div class="flex"> <img class="object-cover w-10 h-10 rounded-full relative " src="${
-//       video.authors[0].profile_picture
-//       } " alt="">&nbsp;  &nbsp;
-//                 <h2 class="card-title">${video.title}</h2>
-//             </div>
-//             <div class="flex">
-//             <p>${video.authors[0].profile_name} </p>
-//             <img src="${
-//       video.authors[0].verified ? `verified.svg` : ""
-//       } " alt="">
-//          </div>
-//           <p class="-ml-14 " >${video.others?.views}</p>
-
-//           <p class=" font-extrabold text-white -mt-20 ml-52  md:ml-96 md:-mt-20 lg:-mt-28 rounded-lg p-2 lg:ml-28 absolute ${isNaN(timeConverter(convertedTime)) ? "" : "hidden"} bg-black "> ${timeConverter(convertedTime)}</p>
-
-//         </div>
-//       </div>
-//         `;
-//     cardContainer.appendChild(div);
-//   });
-// }
-
-const sortByViewButton = document.querySelector("#sort-by-view");
-// console.log(sortByViewButton);
-sortByViewButton.addEventListener("click", () => {
+// const sortByViewButton = document.querySelector("#sort-by-view");
+// // console.log(sortByViewButton);
+// sortByViewButton.addEventListener("click", () => {
+//  sort = sortMaker()
+//   console.log(sort);
+// });
   // console.log(sortByViewButton);
-//  const ck = allCategory.forEach(views)
-// console.log(ck);
-  const sortedVideos = [...fetchedVideos];
-  console.log(fetchedVideos);
-  const makeViewCountToInt =
-    (sortedVideos) 
-  console.log(makeViewCountToInt);
+  //  const ck = allCategory.forEach(views)
+  // console.log(ck);
+  // const sortedVideos = [...fetchedVideos];
+  // console.log(fetchedVideos);
+  // const makeViewCountToInt =
+  //   (sortedVideos)
+  // console.log(makeViewCountToInt);
   // const sortAfterViewCountToInt = makeViewCountToInt.sort(
-    // (a, b) => b.others.views - a.others.views
+  // (a, b) => b.others.views - a.others.views
   // );
   // displayVideos(sortedVideos);
   // console.log();
-});
-
-
-
+// });
 
 function blog() {
   open("blog.html");
 }
-
 
 handleClass(1000);
 handleCategory();
